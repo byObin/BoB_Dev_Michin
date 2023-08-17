4c57025d78b7d0d7c2d39e78383ac4204ea877f7
JiEun Park <53637261+rioju412@users.noreply.github.com>
2023-08-18 07:13:47 +0900

    final_v4

--- mini-game.js
+++ mini-game.js
@@ -1,34 +1,67 @@
-window.onload = function() {
-    let gameCircles = document.querySelectorAll('.gameCircle');
+const bar = document.getElementById('bar');
+const circle = document.getElementById('circle');
+const targetZone = document.getElementById('targetZone');
+const gameResult = document.getElementById('gameResult');
+const resultImage = document.getElementById('resultImage');
+const resultText = document.getElementById('resultText');
 
-    gameCircles.forEach(gameCircle => {
-        gameCircle.addEventListener('click', function() {
-            const speed = parseInt(gameCircle.getAttribute('data-speed'));
-            const targetSize = parseInt(gameCircle.getAttribute('data-targetSize'));
-            const failMessage = gameCircle.getAttribute('data-failMessage');
+let direction = 1;  // 1: 오른쪽, -1: 왼쪽
+let interval;
+let circlePosition = 0;
+let currentFailMessage;
 
-            // Open a new window for the mini-game.
-            const miniGameWindow = window.open('mini-game.html', '_blank', 'width=800,height=600');
-            
-            miniGameWindow.onload = function() {
-                // Set up the mini game using the parameters from the main game
-                miniGameWindow.startGame(speed, targetSize, failMessage);
-            };
-        });
+function moveCircle(speed) {
+    circlePosition += (speed * direction);
 
-        let randomTop = (window.innerHeight * 0.35) + (Math.random() * (window.innerHeight * 0.55));
-        let randomLeft = Math.random() * (window.innerWidth * 0.6);
+    if (circlePosition > (bar.offsetWidth - circle.offsetWidth)) {
+        circlePosition = bar.offsetWidth - circle.offsetWidth;
+        direction *= -1;
+    } else if (circlePosition < 0) {
+        circlePosition = 0;
+        direction *= -1;
+    }
 
-        gameCircle.style.top = randomTop + 'px';
-        gameCircle.style.left = randomLeft + 'px';
-    });
+    circle.style.left = circlePosition + 'px';
+}
+
+function checkGameResult(failMessage) {
+    const circleRight = circlePosition + circle.offsetWidth;
+    const targetLeft = targetZone.offsetLeft;
+    const targetRight = targetLeft + targetZone.offsetWidth;
+
+    clearInterval(interval);  // 게임 중지
+
+    gameResult.style.display = 'block';  // 결과 화면 표시
+
+    if (circlePosition < targetRight && circleRight > targetLeft) {
+        resultImage.src = '/images/memory.png';
+        resultText.innerText = '물리 메모리 획득!';
+        return 'success';
+    } else {
+        resultImage.src = '/images/error.png';
+        resultText.innerText = failMessage;
+        return 'fail';
+    }
+}
+
+function setTargetSize(size) {
+    targetZone.style.width = size + 'px';
+    targetZone.style.left = (bar.offsetWidth - size) / 2 + 'px';  // 중앙에 위치시킴
+}
+
+function startGame(speed, targetSize, failMessage) {
+    setTargetSize(targetSize);
+    circlePosition = 0;
+    circle.style.left = circlePosition + 'px';
+    interval = setInterval(() => moveCircle(speed), 50);
+    currentFailMessage = failMessage; // 실패 메시지를 저장합니다.
 }
 
 document.body.addEventListener('keydown', (e) => {
     if (e.code === 'Space') {
-        gameCircles.forEach(gameCircle => {
-            const failMessage = gameCircle.getAttribute('data-failMessage');
-            checkGameResult(failMessage);
-        });
+        checkGameResult(currentFailMessage); // 저장된 failMessage를 사용합니다.
     }
+});
+document.getElementById('backToMain').addEventListener('click', function() {
+    window.close();
 });
